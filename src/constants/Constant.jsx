21bffb69088241e6ccc6cfc14c72fa76cc4a24a3
Vaibhav Bhardwaj <vaibhavbhardwaj439@gmail.com>
2025-08-12 18:48:47 +0530

    Update Constant.jsx

--- src/constants/Constant.jsx
+++ src/constants/Constant.jsx
@@ -8,11 +8,11 @@ import Javascript from "../assets/Javascript.png";
 import Git from "../assets/Git.png";
 import Vite from "../assets/Vite.png";
 import TailwindCSS from "../assets/TailwindCSS.png";
-import react from "../assets/react.png";
-import Vue from "../assets/vue.png";
+import react from "../assets/React.png";
+import Vue from "../assets/Vue.png";
 
 import Hackthechain from "../assets/HackTheChainLogo.png";
-import GSSOC from "../assets/GSSoc.jpg";
+import GSSOC from "../assets/GSSoC.jpg";
 import IITB from "../assets/IITB.png";
 import Sports from "../assets/Sports_Club.png";
 import IIITians from "../assets/IIITians_Network.png";
